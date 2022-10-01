8adb02e7bb8d0b073b24d46c68adb5c6f481fa4b
kdmontero <kevindmontero@gmail.com>
2022-10-01 16:47:38 +0800

    set intents.members to True

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ from help import CustomHelp
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 bot = commands.Bot(
     command_prefix=COMMAND_PREFIX,
     intents=intents,
